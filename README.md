dc097c435fd52e0638310eb865aeea99155cbebc
demir <46607042+swordfishtr@users.noreply.github.com>
2024-09-24 01:47:37 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,31 @@
+# PS Format Overwrite
+
+Browser extension that overwrites the format of the currently viewed team on Pokemon Showdown's teambuilder. This is most useful for old metagames that retain teambuilder support but aren't present in the given list of formats. Skips the part of teambuilding where you have to scroll down to view the legal Pokemon for your tier. You can duplicate the team afterwards - there's no need to use this extension for every new team.
+
+The method used is provided by the Showdown client, so even when the formatting of stored teams changes, this extension should remain perfectly functional.
+
+There's a safety measure in place to prevent you from corrupting the teambuilder. Still, if you're going to mess around, backup your teams first!
+
+## Usage
+
+Valid format names consist of only numbers and lowercase letters. Examples: `gen9ou` `gen8bdspnu` `gen7letsgoru`
+
+There's no list of 'Every Format Ever' - they're constantly added and removed. Start here for a lot of (but not all) formats with teambuilder support: https://github.com/smogon/pokemon-showdown/blob/master/config/formats.ts
+
+1. Navigate to the team whose format you wish to overwrite, or create a new one.
+
+2. Open the extension's popup, type in the desired format name and click OK!
+
+3. If successful, the popup will close itself. The change is immediate and nothing else needs to be done.
+
+## Install instructions
+
+This extension has cross-browser compatibility.
+
+1. Download as zip and then unzip.
+
+2. Chromium users: go to `chrome://extensions/` and enable Developer mode, then Load unpacked and select the directory that has manifest.json in it.
+
+2. Firefox users: go to `about:debugging#/runtime/this-firefox` then Load Temporary Add-on... and select manifest.json directly.
+
+3. Chrome users should disable the extension after they're done with it. Firefox will do this on shutdown.
---
+++ popup.js
@@ -0,0 +1,54 @@
+// Polyfill for browser compatibility
+if (typeof browser === "undefined") globalThis.browser = chrome;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const formatInput = document.getElementById("format-input");
+    const formatButton = document.getElementById("format-button");
+    const formatError = document.getElementById("format-error");
+
+    /* Get the currently viewed tab and send the function as script.
+    This is the only method for executing a content script in the MAIN context
+    and still communicating with it (albeit limited) in modern browsers. */
+
+    formatButton.addEventListener("click", () => {
+        if(/[\]\|\/]/.test(formatInput.value)) {
+            showMessage("These characters corrupt the teambuilder ] | /");
+            return;
+        }
+        browser.tabs.query({
+            active: true,
+            currentWindow: true
+        }).then(tab => {
+            browser.scripting.executeScript({
+                target: {tabId: tab[0].id},
+                func: overwriteFormat,
+                args: [formatInput.value],
+                world: "MAIN"
+            }).then((out) => {
+                // 0 = ok, 1 = fail, else error
+                if(out[0].result === 0) window.close();
+                else if(out[0].result === 1) showMessage("Couldn't find a team here.");
+                else showMessage("Something went wrong! Send current url and this to dev:<br>" + out[0].message);
+            }).catch(() => showMessage("Extensions can't access browser-protected pages like the one in this tab."));
+        });
+    });
+
+    function showMessage(message) {
+        formatInput.style.display = "none";
+        formatButton.style.display = "none";
+        formatError.style.display = "block";
+        formatError.innerHTML = message;
+    }
+});
+
+function overwriteFormat(format) {
+    try {
+        if(window.app?.curRoom?.curTeam) {
+            app.curRoom.changeFormat(format);
+            return 0;
+        } else return 1;
+    } catch(err) {
+        console.log("PS FORMAT OVERWRITE: " + err);
+        return err;
+    }
+}
